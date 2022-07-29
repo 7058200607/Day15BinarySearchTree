d8dbd31aca68fd895dd1b8844e5e18d212f54787
Vrushali Joshi <vrushalijoshi589@gmail.com>
2022-07-29 15:24:54 +0530

    Ability to create a binary search tree

---
+++ Day15BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15BinarySearchTree
+{
+    public class BinarySearchTree<T> where T : IComparable<T>
+    {
+        public T NodeData { get; set; }
+        public BinarySearchTree<T> LeftTree { get; set; }
+        public BinarySearchTree<T> RightTree { get; set; }
+        public BinarySearchTree(T nodeData)
+        {
+            this.NodeData = nodeData;
+            this.LeftTree = null;
+            this.RightTree = null;
+        }
+        int LeftCount = 0, RightCount = 0;
+        bool result = false;
+        public void Insert(T item)
+        {
+            T CurrentNodeValue = this.NodeData;
+
+            if ((CurrentNodeValue.CompareTo(item)) == 0)
+            {
+                if (this.LeftTree == null)
+                {
+                    this.LeftTree = new BinarySearchTree<T>(item);
+                }
+                else
+                {
+                    this.LeftTree.Insert(item);
+                }
+            }
+
+
+
+            else
+            {
+                if (this.RightTree == null)
+                    this.RightTree = new BinarySearchTree<T>(item);
+                else
+                {
+                    this.RightTree.Insert(item);
+                }
+            }
+        }
+        public void Display()
+        {
+            if (this.LeftTree != null)
+            {
+                this.LeftCount++;
+                this.LeftTree.Display();
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if (this.RightTree != null)
+            {
+                this.RightCount++;
+                this.RightTree.Display();
+            }
+        }
+    }
+}
---
+++ Day15BinarySearchTree/Program.cs
@@ -0,0 +1,24 @@
+﻿using Day15BinarySearchTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Binary Search Tree");
+            BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>(56);
+
+            binarySearchTree.Insert(30);
+            binarySearchTree.Insert(70);
+
+            binarySearchTree.Display();
+        }
+
+    }
+}
